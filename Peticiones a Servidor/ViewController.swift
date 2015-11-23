be21404b306db913232f3bae13759144de845c41
Mateo <Mateo@pao.local>
2015-11-22 23:34:42 -0500

    ISBN App

--- Peticiones a Servidor/ViewController.swift
+++ Peticiones a Servidor/ViewController.swift
@@ -9,17 +9,135 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var textField: UITextField!
+   
+    @IBAction func usuarioTexto(sender: AnyObject) {
+        
+        var wasSuccesful = false
+        
+        let attemptedUrl = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + textField.text! + "")
+        
+        // Downloading the web content from the url
+        
+        if let url = attemptedUrl {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+                
+                if let urlContent = data {
+                    
+                    let webContent =  NSString(data: urlContent, encoding: NSUTF8StringEncoding)!
+                    
+                    print(webContent)
+                    
+                    //  (UserInterface) Dispatch to fast reaction
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        
+                        wasSuccesful = true
+                        
+                       self.textView.text = webContent as String
+                    })
 
+                    if wasSuccesful == false {
+                        
+                    // Error if the city does not exist or the procces failed
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            
+                            self.errorLabel.text = "No se encontro el libro! Intentalo de nuevo."
+                            
+                        })
+                    }
+                }
+            }
+            
+            task.resume()
+            // Error if the user enters invalid symbols
+        } else {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.errorLabel.text = "Por favor ingresa un ISBN valido."
+            })
+        }
+    }
+        /*
+        let attemptedUrl = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + textField.text! + "")
+        
+        if let url = attemptedUrl {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+                
+                if let urlContent = data {
+                    
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)!
+                    
+                    print(webContent)
+                    
+                    //  (UserInterface) Dispatch to fast reaction
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        
+                        self.textView.text = webContent as String
+                    })
+                    
+                    if wasSuccesful == false {
+                        
+                        
+                        // Error if the city does not exist or the procces failed
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            
+                            self.errorLabel.text = "No se pudo encontrar el libro! Porfavor intentalo de nuevo."
+                        })
+                    }
+                    
+                } else {
+                }
+            }
+            task.resume()
+        }
+
+    }
+    
+    
+    // Action when fisnish editing not when "Search" button is triggered
+    
+    @IBAction func usuarioISBN(sender: UITextField) {
+        
+        let attemptedUrl = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + textField.text! + "")
+        
+        if let url = attemptedUrl {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+                
+                if let urlContent = data {
+                    
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)!
+                    
+                    print(webContent)
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        
+                    self.textView.text = webContent as String
+                    })
+                    
+                } else {
+                }
+            }
+            task.resume()
+        }
+    }
+    */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
